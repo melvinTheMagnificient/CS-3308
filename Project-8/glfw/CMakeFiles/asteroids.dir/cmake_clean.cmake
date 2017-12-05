80c13350910da55746117ecd6e2b9ca31a5fe912
melvinTheMagnificient <mcbluejeans@gmail.com>
2017-12-04 17:22:38 -0700

    Add files via upload

---
+++ Project-8/glfw/CMakeFiles/asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asteroids.dir/main.cpp.o"
+  "CMakeFiles/asteroids.dir/Game.cpp.o"
+  "CMakeFiles/asteroids.dir/deps/glad.c.o"
+  "asteroids.pdb"
+  "asteroids"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
